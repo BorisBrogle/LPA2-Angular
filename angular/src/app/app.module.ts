a7269d0e8739f9d8ec4c8a66e05efd09390bc3db
boris.brogle <boris.brogle@gmail.com>
2017-12-16 06:14:45 -0500

    Hacker news added

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { NewsService } from './news.service';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    NewsDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NewsService],
+  bootstrap: [NewsComponent]
 })
 export class AppModule { }
